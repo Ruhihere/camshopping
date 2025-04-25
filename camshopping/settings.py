1450b1047a9d01cb31b7c7a3057d57808e15d24f
Ruhihere <ruhiparween0502@gmail.com>
2025-04-25 15:00:16 +0530

    Configured for deployment

--- camshopping/settings.py
+++ camshopping/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-stlq+th%s$yod1#9j@v-&yeyyw9jbuc@$pfrfqvrjp30*r&6)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['camshopping.onrender.com']
 
 
 # Application definition
